a274afd65b33e05852e947da7e88f8135acc80c3
Bhagyesh-vyas-009 <bhagyeshvyas009@gmail.com>
2024-12-11 22:53:56 +0530

    GetAllCountry and CountryDelete API Consumed

---
+++ ApiConsume/Controllers/CountryController.cs
@@ -0,0 +1,63 @@
+﻿using ApiConsume.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiConsume.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        Uri baseAddress = new Uri("http://localhost:41349/api");
+
+        public CountryController()
+        {
+            _httpClient=new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CountryList()
+        {
+            List<CountryModel> countries = new List<CountryModel>();
+            HttpResponseMessage response=_httpClient.GetAsync($"{_httpClient.BaseAddress}/Country").Result;
+            if (response.IsSuccessStatusCode) { 
+                string data=response.Content.ReadAsStringAsync().Result;
+                dynamic jsonObject=JsonConvert.DeserializeObject<dynamic>(data);
+                var extractedDataJson = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+
+                countries=JsonConvert.DeserializeObject<List<CountryModel>>(extractedDataJson);
+            }
+            return View("CountryList",countries);
+        }
+
+        //[HttpDelete("{CountryID}")]
+        public async Task<IActionResult> CountryDelete(int CountryID) {
+            try
+            {
+
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/Country/{CountryID}");
+            //HttpResponseMessage response = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/Country/{CountryID}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        TempData["Success"] = "Country deleted successfully.";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = $"Failed to delete the Country. Status Code: {response.StatusCode}";
+                    }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"An error occurred: {ex.Message}";
+            }
+
+            return RedirectToAction("CountryList");
+        }
+    }
+}
---
+++ ApiConsume/Models/CountryModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace ApiConsume.Models
+{
+    public class CountryModel
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public string CountryCode { get; set; }
+
+        //[JsonConverter(typeof(JsonDateTimeConverter))]
+        public DateTime? CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
